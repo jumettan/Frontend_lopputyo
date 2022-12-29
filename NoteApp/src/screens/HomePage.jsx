337b0a568e327ed448050e409dc505713107f036
jumettan <juho.mettanen@student.samk.fi>
2022-12-29 19:59:57 +0200

    Update HomePage.jsx

--- NoteApp/src/screens/HomePage.jsx
+++ NoteApp/src/screens/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>Hello! <br /> <br />
-        Welcome to the NotesApp! with this app you will be able to add new courses and create new notes for them.
+        Welcome to the NotesApp! With this app you will be able to add new courses and create new notes for them.
         
     </div>
   )
